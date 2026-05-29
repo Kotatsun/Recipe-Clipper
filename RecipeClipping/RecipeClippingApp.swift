a3907fe5432acc8d9e2f7a9b6dd3aa683c5ce9f3
Kotatsun <shine_of_light000@yahoo.co.jp>
2026-05-29 23:12:36 +0900

    Initial Commit

---
+++ RecipeClipping/RecipeClippingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeClippingApp.swift
+//  RecipeClipping
+//
+//  Created by こたつん on 2026/05/29.
+//
+
+import SwiftUI
+
+@main
+struct RecipeClippingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
